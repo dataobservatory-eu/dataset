799613cb6cba2363ca9c271f7b45ec46f5d783f2
antaldaniel <antaldaniel@gmail.com>
2024-01-27 10:32:27 +0100

    testthat::expect_true(is.finite.POSIXlt(provenance(iris_dataset)$started_at)) changed because it threw a testing error on R oldreleases (Windows, Mac)

--- tests/testthat/test-dataset_prov.R
+++ tests/testthat/test-dataset_prov.R
@@ -1,9 +1,8 @@
 
 
-
 test_that("provenance() works", {
-  expect_true(is.finite.POSIXlt(provenance(iris_dataset)$started_at))
-  expect_true(is.finite.POSIXlt(provenance(iris_dataset)$ended_at))
+  expect_true(inherits(provenance(iris_dataset)$started_at, "POSIXct"))
+  expect_true(inherits(provenance(iris_dataset)$ended_at, "POSIXct"))
   expect_true(grepl("doi:", provenance(iris_dataset)$wasAssocitatedWith))
   expect_equal(provenance(iris_dataset)$wasInformedBy, datasource_get(iris_dataset))
 })
