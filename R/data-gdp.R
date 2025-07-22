b7b5777d7e49b3bb09340464e66a54b1390d26fc
Daniel Antal <antaldaniel@gmail.>
2025-07-22 19:26:56 +0200

    New example dataset for documentation, examples, and unit testing

---
+++ R/data-gdp.R
@@ -0,0 +1,16 @@
+#' A Small GDP Dataset
+#'
+#' A subset of data from the Eurostat's GDP and main aggregates -
+#' international data cooperation annual data
+#'
+#' @format ## `gdp`
+#' A data frame with 10 rows and 5 columns:
+#' \describe{
+#'   \item{geo}{Country name}
+#'   \item{year}{Reference time period}
+#'   \item{gdp}{Gross Domestic Product}
+#'   \item{unit}{Unit of values}
+#'   \item{freq}{Frequency}
+#' }
+#' @source <https://doi.org/10.2908/NAIDA_10_GDP>
+"gdp"
