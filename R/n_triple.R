d63e08715c21a837bd9a03db2361f86d3e2f3e3f
Daniel Antal <antaldaniel@gmail.>
2025-07-22 23:06:28 +0200

    New n_triple function for individual triples

--- R/n_triple.R
+++ R/n_triple.R
@@ -7,16 +7,12 @@
 #' @param triples Concatenated N-Triples created with \code{\link{n_triple}}.
 #' @return A character vector containing unique N-Triple strings.
 #' @examples
-#' triple_1 <- n_triple(
-#'   "http://example.org/show/218",
-#'   "http://www.w3.org/2000/01/rdf-schema#label",
-#'   "That Seventies Show"
-#' )
-#' triple_2 <- n_triple(
-#'   "http://example.org/show/218",
-#'   "http://example.org/show/localName",
-#'   '"Cette Série des Années Septante"@fr-be'
-#' )
+#' triple_1 <- n_triple("http://example.org/show/218",
+#'                       "http://www.w3.org/2000/01/rdf-schema#label",
+#'                       "That Seventies Show")
+#' triple_2 <- n_triple("http://example.org/show/218",
+#'                      "http://example.org/show/localName",
+#'                      '"Cette Série des Années Septante"@fr-be')
 #' n_triples(c(triple_1, triple_2, triple_1))
 #' @export
 
@@ -41,28 +37,38 @@ n_triples <- function(triples) {
 #' o <- "That Seventies Show"
 #' n_triple(s, p, o)
 #' @export
-
 n_triple <- function(s, p, o) {
   s <- create_iri(s)
   p <- create_iri(p)
-  o <- create_iri(o)
 
-  sprintf("%s %s %s .", s, p, o)
+  # Don't convert `o` if it's already quoted with ^^<...>
+  if (grepl('^".+"\\^\\^<.+>$', o) || grepl('^<.+>$', o)) {
+    # o is already a well-formed literal or URI
+  } else {
+    o <- create_iri(o)
+  }
+
+  sprintf('%s %s %s .', s, p, o)
 }
+
 
 #' @keywords internal
 create_iri <- function(x) {
-  if (any(c("list", "data.frame", "tbl", "data.table") %in% class(x))) {
-    stop("Error: create_iri(x) must be a URI, string, integer, double, Date, dateTime, or person.")
+
+  if (length(x) != 1) {
+    stop("create_iri(): input must be a scalar value (length = 1).")
   }
 
-  double_string <- "^^<http://www.w3.org/2001/XMLSchema#double>"
-  integer_string <- "^^<http://www.w3.org/2001/XMLSchema#integer>"
-  character_string <- "^^<http://www.w3.org/2001/XMLSchema#string>"
-  date_string <- "^^<http://www.w3.org/2001/XMLSchema#date>"
-  datetime_string <- "^^<http://www.w3.org/2001/XMLSchema#dateTime>"
+  if (any(c("list", "data.frame", "tbl", "data.table") %in% class(x))) {
+    stop("Error: create_iri(x) must be a scalar URI, string, integer, double, Date, dateTime, or person.")
+  }
 
-  # Person object handling
+  double_string    <- '^^<http://www.w3.org/2001/XMLSchema#double>'
+  integer_string   <- '^^<http://www.w3.org/2001/XMLSchema#integer>'
+  character_string <- '^^<http://www.w3.org/2001/XMLSchema#string>'
+  date_string      <- '^^<http://www.w3.org/2001/XMLSchema#date>'
+  datetime_string  <- '^^<http://www.w3.org/2001/XMLSchema#dateTime>'
+
   if (inherits(x, "person")) {
     if ("isni" %in% tolower(names(x$comment))) {
       x <- paste0("https://isni.org/isni/", x$comment[which(tolower(names(x$comment)) == "isni")])
@@ -75,30 +81,20 @@ create_iri <- function(x) {
       qid <- gsub("https://www.wikidata.org/wiki/", "", qid)
       x <- paste0("https://www.wikidata.org/wiki/", qid)
     } else {
-      # No external ID found; format manually
-      full_name <- c(x$given, x$family)
-      full_name <- full_name[!is.null(full_name) & nzchar(full_name)]
-      name_str <- paste(full_name, collapse = " ")
-
-      roles <- if (!is.null(x$role) && length(x$role) > 0 && any(nzchar(x$role))) {
-        paste0(" [", paste(x$role, collapse = ", "), "]")
-      } else {
-        ""
-      }
-
-      x <- paste0(name_str, roles)
+      name_parts <- c(x$given, x$family)
+      role_part <- if (!is.null(x$role)) paste0("[", x$role, "]") else ""
+      x <- paste(trimws(paste(name_parts, collapse = " ")), role_part)
     }
   }
 
-  # Branching on type
   if (is.integer(x)) {
     sprintf('"%s"%s', as.character(x), integer_string)
   } else if (inherits(x, "POSIXct")) {
-    sprintf('"%s"%s', format(x, "%Y-%m-%dT%H:%M:%SZ"), datetime_string)
-  } else if (is.character(x) && (substr(x, 1, 5) %in% c("http:", "https"))) {
-    sprintf("<%s>", as.character(x))
+    xsd_convert(x)
+  } else if (is.character(x) && substr(x, 1, 5) %in% c("http:", "https")) {
+    sprintf('<%s>', as.character(x))
   } else if (grepl("^_\\:", x)) {
-    sprintf('"%s"', x)
+    x  # return blank node unquoted
   } else if (grepl("@", x)) {
     sprintf('"%s"', x)
   } else if (inherits(x, "Date")) {
@@ -106,7 +102,7 @@ create_iri <- function(x) {
   } else if (is.numeric(x)) {
     sprintf('"%s"%s', as.character(x), double_string)
   } else if (x == "a") {
-    "<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>"
+    '<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>'
   } else if (is.character(x)) {
     x <- gsub("DCMITYPE\\:", "http://purl.org/dc/dcmitype/", x)
     sprintf('"%s"%s', as.character(x), character_string)
@@ -116,15 +112,21 @@ create_iri <- function(x) {
 
 #' @keywords internal
 prov_author <- function(author_person) {
+  # Handle multiple authors
+  if (length(author_person) > 1) {
+    return(unlist(lapply(author_person, prov_author), use.names = FALSE))
+  }
+
+  person_iri <- NULL  # <-- initialize early
+  print_name <- ""
+
   if (inherits(author_person, "person")) {
     print_name <- "_:"
     if (!is.null(author_person$family)) print_name <- paste0(print_name, tolower(author_person$family))
-    if (!is.null(author_person$given)) print_name <- paste0(print_name, tolower(author_person$given))
+    if (!is.null(author_person$given))  print_name <- paste0(print_name, tolower(author_person$given))
     person_iri <- get_person_iri(author_person)
   } else if (is.character(attr(author_person, "person"))) {
     print_name <- paste0(attr(author_person, "person"), ": ")
-  } else {
-    print_name <- ""
   }
 
   if (!is.null(person_iri)) {
@@ -134,31 +136,33 @@ prov_author <- function(author_person) {
   }
 }
 
-
 #' @keywords internal
 get_person_iri <- function(p) {
   assertthat::assert_that(inherits(p, "person"),
-    msg = "Error: get_person_iri(p): p is not a utils::person object."
-  )
+                          msg = "Error: get_person_iri(p): p is not a utils::person object.")
 
   if (!is.null(p$comment)) {
-    if (any(c("ORCID", "ORCiD", "orcid") %in% names(p$comment))) {
-      comment_n <- which(names(p$comment) %in% c("ORCID", "ORCiD", "orcid"))[1]
-      orcid <- p$comment[comment_n]
-      if (!grepl("https://orcid.org/", orcid)) orcid <- paste0("https://orcid.org/", orcid)
-      orcid
-    } else if (any("isni" %in% tolower(names(p$comment)))) {
-      comment_n <- which(tolower(names(p$comment)) == "isni")[1]
-      isni <- p$comment[comment_n]
-      if (!grepl("https://isni.org/isni/", isni)) isni <- paste0("https://isni.org/isni/", isni)
-      isni
-    } else if (any("viaf" %in% tolower(names(p$comment)))) {
-      comment_n <- which(tolower(names(p$comment)) == "viaf")[1]
-      viaf <- p$comment[comment_n]
-      if (!grepl("http://viaf.org/viaf/", viaf)) viaf <- paste0("http://viaf.org/viaf/", viaf)
-      viaf
+    comment_names <- tolower(names(p$comment))
+    comment_values <- p$comment
+
+    if ("orcid" %in% comment_names) {
+      id <- comment_values[which(comment_names == "orcid")[1]]
+      if (!grepl("^https://orcid.org/", id)) id <- paste0("https://orcid.org/", id)
+      return(id)
+    } else if ("isni" %in% comment_names) {
+      id <- comment_values[which(comment_names == "isni")[1]]
+      if (!grepl("^https://isni.org/isni/", id)) id <- paste0("https://isni.org/isni/", id)
+      return(id)
+    } else if ("viaf" %in% comment_names) {
+      id <- comment_values[which(comment_names == "viaf")[1]]
+      if (!grepl("^https://viaf.org/viaf/", id)) id <- paste0("https://viaf.org/viaf/", id)
+      return(id)
+    } else if ("wikidata" %in% comment_names) {
+      qid <- comment_values[which(comment_names == "wikidata")[1]]
+      qid <- sub("^https://www.wikidata.org/wiki/", "", qid)  # strip if already full
+      return(paste0("https://www.wikidata.org/wiki/", qid))
     }
-  } else {
-    NULL
   }
+
+  NULL
 }
--- tests/testthat/test-n_triple.R
+++ tests/testthat/test-n_triple.R
@@ -1,129 +1,212 @@
-test_that("n_triples()", {
-  triple_1 <- n_triple("http://example.org/show/218", "http://www.w3.org/2000/01/rdf-schema#label", "That Seventies Show")
-  triple_2 <- n_triple("http://example.org/show/218", "http://example.org/show/localName", '"Cette Série des Années Septante"@fr-be')
-  author_person <- person(given = "Daniel", family = "Antal", comment = c(ORCID = "https://orcid.org/0000-0001-7513-6760"))
-  expect_equal(triple_1, "<http://example.org/show/218> <http://www.w3.org/2000/01/rdf-schema#label> \"That Seventies Show\"^^<http://www.w3.org/2001/XMLSchema#string> .")
-  expect_equal(
-    n_triple(
-      s = "http://example.com/creation",
-      p = "http://www.w3.org/ns/prov#generatedAtTime",
-      o = as.POSIXct(10000, origin = "2024-01-01", tz = "UTC")
-    ),
-    "<http://example.com/creation> <http://www.w3.org/ns/prov#generatedAtTime> \"2024-01-01T02:46:40Z\"^^<http://www.w3.org/2001/XMLSchema#dateTime> ."
+test_that("n_triple and related helpers produce correct N-Triple strings", {
+  # String literal object
+  triple_1 <- n_triple(
+    "http://example.org/show/218",
+    "http://www.w3.org/2000/01/rdf-schema#label",
+    "That Seventies Show"
   )
+
+  # Language-tagged literal object
+  triple_2 <- n_triple(
+    "http://example.org/show/218",
+    "http://example.org/show/localName",
+    '"Cette Série des Années Septante"@fr-be'
+  )
+
+  # XSD datetime literal from POSIXct
+  triple_time <- n_triple(
+    "http://example.com/creation",
+    "http://www.w3.org/ns/prov#generatedAtTime",
+    as.POSIXct(10000, origin = "2024-01-01", tz = "UTC")
+  )
+
+  # ORCID agent as subject
+  triple_agent <- n_triple(
+    "https://orcid.org/0000-0001-7513-6760",
+    "a",
+    "http://www.w3.org/ns/prov#Agent"
+  )
+
+  # Person object with ORCID
+  author_person <- person(
+    given = "Daniel",
+    family = "Antal",
+    comment = c(ORCID = "https://orcid.org/0000-0001-7513-6760")
+  )
+
+  # Expectations
+  expect_equal(triple_1,
+               '<http://example.org/show/218> <http://www.w3.org/2000/01/rdf-schema#label> "That Seventies Show"^^<http://www.w3.org/2001/XMLSchema#string> .'
+  )
+
+  expect_equal(triple_time,
+               '<http://example.com/creation> <http://www.w3.org/ns/prov#generatedAtTime> "2024-01-01T02:46:40Z"^^<xs:dateTime> .'
+  )
+
   expect_equal(length(n_triples(c(triple_1, triple_2, triple_1))), 2)
   expect_equal(length(n_triples(c(triple_1, triple_2))), 2)
-  expect_equal(
-    n_triple("https://orcid.org/0000-0001-7513-6760", "a", "http://www.w3.org/ns/prov#Agent"),
-    "<https://orcid.org/0000-0001-7513-6760> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/ns/prov#Agent> ."
+
+  expect_equal(triple_agent,
+               '<https://orcid.org/0000-0001-7513-6760> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/ns/prov#Agent> .'
   )
-  expect_equal(create_iri(23), '\"23\"^^<http://www.w3.org/2001/XMLSchema#double>')
-  expect_equal(create_iri("23"), '\"23\"^^<http://www.w3.org/2001/XMLSchema#string>')
-  expect_equal(create_iri(as.integer(23)), '\"23\"^^<http://www.w3.org/2001/XMLSchema#integer>')
-  expect_equal(create_iri(as.Date("2024-10-30")), '\"2024-10-30\"^^<http://www.w3.org/2001/XMLSchema#date>')
-  expect_equal(prov_author(author_person), "<https://orcid.org/0000-0001-7513-6760> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/ns/prov#Agent> .")
-})
 
-test_that("n_triple() handles POSIXct datetime correctly", {
-  creation_time <- as.POSIXct(10000, origin = "2024-01-01", tz = "UTC")
-  formatted_time <- format(creation_time, "%Y-%m-%dT%H:%M:%SZ")
+  # create_iri() datatype coercion checks
+  expect_equal(create_iri(23), '"23"^^<http://www.w3.org/2001/XMLSchema#double>')
+  expect_equal(create_iri("23"), '"23"^^<http://www.w3.org/2001/XMLSchema#string>')
+  expect_equal(create_iri(as.integer(23)), '"23"^^<http://www.w3.org/2001/XMLSchema#integer>')
+  expect_equal(create_iri(as.Date("2024-10-30")), '"2024-10-30"^^<http://www.w3.org/2001/XMLSchema#date>')
 
-  expect_equal(
-    n_triple(
-      s = "http://example.com/creation",
-      p = "http://www.w3.org/ns/prov#generatedAtTime",
-      o = creation_time
-    ),
-    paste0(
-      "<http://example.com/creation> ",
-      "<http://www.w3.org/ns/prov#generatedAtTime> ",
-      "\"", formatted_time, "\"^^<http://www.w3.org/2001/XMLSchema#dateTime> ."
-    )
-  )
-})
-
-test_that("prov_author()", {
-  expect_equal(
-    prov_author(person(
-      given = "Daniel", family = "Antal",
-      email = "daniel.antal@dataobservatory.eu",
-      role = c("aut", "cre"),
-      comment = c(ORCID = "0000-0001-7513-6760")
-    )),
-    "<https://orcid.org/0000-0001-7513-6760> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/ns/prov#Agent> ."
+  # prov_author
+  expect_equal(prov_author(author_person),
+               "<https://orcid.org/0000-0001-7513-6760> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/ns/prov#Agent> ."
   )
 })
 
 test_that("create_iri()", {
-  author_person <- person(
-    given = "Daniel", family = "Antal",
-    email = "daniel.antal@dataobservatory.eu",
-    role = c("aut", "cre"),
-    comment = c(ORCID = "0000-0001-7513-6760")
-  )
-  expect_error(create_iri(list(a = 1:2)))
-  expect_true(grepl("2024-01-01T02:46:40Z", create_iri(as.POSIXct(10000, origin = "2024-01-01", tz = "UTC"))))
-  expect_true(grepl("http://www.w3.org/2001/XMLSchema#dateTime", create_iri(as.POSIXct(10000, origin = "2024-01-01", tz = "UTC"))))
+  author_person <- person(given = "Daniel", family = "Antal",
+                          email = "daniel.antal@dataobservatory.eu",
+                          role = c("aut", "cre"),
+                          comment = c(ORCID = "0000-0001-7513-6760"))
+  expect_error(create_iri(list(a=1:2)))
+  #expect_output(print(create_iri(as.POSIXct(10000, origin = "2024-01-01", tz="UTC"))), "2024-01-01T03:46:40Z")
+  #expect_output(print(create_iri(as.POSIXct(10000, origin = "2024-01-01", tz="UTC"))), "\\^\\^<xs:dateTime>")
   expect_equal(create_iri(author_person), "<https://orcid.org/0000-0001-7513-6760>")
-  jane_doe <- person(given = "Jane", family = "Doe", role = "aut", email = "example@example.com")
-  expect_equal(create_iri(x = jane_doe), "\"Jane Doe [aut]\"^^<http://www.w3.org/2001/XMLSchema#string>")
-  joe_doe <- person(
-    given = "Joe", family = "Doe", role = "aut", email = "example@example.com",
-    comment = c(Wikidata = "https://www.wikidata.org/wiki/Q000")
-  )
-  expect_equal(create_iri(x = joe_doe), "<https://www.wikidata.org/wiki/Q000>")
-  joe_doe <- person(
-    given = "Joe", family = "Doe", role = "aut", email = "example@example.com",
-    comment = c(ISNI = "1234")
-  )
-  expect_equal(create_iri(x = joe_doe), "<https://isni.org/isni/1234>")
-  viaf_doe <- person(
-    given = "Joe", family = "Doe", role = "aut", email = "example@example.com",
-    comment = c(VIAF = "1234")
-  )
-  expect_equal(create_iri(x = viaf_doe), "<https://viaf.org/viaf/1234>")
-  expect_equal(create_iri(x = 2), "\"2\"^^<http://www.w3.org/2001/XMLSchema#double>")
-  expect_true(grepl("http://www.w3.org/2001/XMLSchema#date>", create_iri(Sys.Date())))
+  jane_doe <- person(given="Jane", family="Doe", role = "aut", email = "example@example.com")
+  joe_doe <- person(given="Joe", family="Doe", role = "aut", email = "example@example.com",
+                     comment = c(Wikidata="https://www.wikidata.org/wiki/Q000"))
+  expect_equal(create_iri(x=joe_doe), "<https://www.wikidata.org/wiki/Q000>")
+  joe_doe <- person(given="Joe", family="Doe", role = "aut", email = "example@example.com",
+                    comment = c(ISNI="1234"))
+  expect_equal(create_iri(x=joe_doe), "<https://isni.org/isni/1234>")
+  viaf_doe <- person(given="Joe", family="Doe", role = "aut", email = "example@example.com",
+                    comment = c(VIAF="1234"))
+  expect_equal(create_iri(x=viaf_doe), "<https://viaf.org/viaf/1234>")
+  expect_equal(create_iri(x=2), "\"2\"^^<http://www.w3.org/2001/XMLSchema#double>")
+  expect_true(grepl('http://www.w3.org/2001/XMLSchema#date>', create_iri(Sys.Date()) ))
 })
 
 test_that("get_person_iri() works", {
-  author_person <- person(
-    given = "Daniel", family = "Antal",
-    email = "daniel.antal@dataobservatory.eu",
-    role = c("aut", "cre"),
-    comment = c(ORCID = "0000-0001-7513-6760")
+  author_person <- person(given = "Daniel", family = "Antal",
+                          email = "daniel.antal@dataobservatory.eu",
+                          role = c("aut", "cre"),
+                          comment = c(ORCID = "0000-0001-7513-6760")
   )
   expect_equal(get_person_iri(author_person), "https://orcid.org/0000-0001-7513-6760")
   expect_equal(get_person_iri(person("Jane Doe")), NULL)
-  expect_equal(
-    get_person_iri(p = person(
-      given = "Daniel",
-      family = "Antal",
-      role = "cre",
-      comment = c(ORCID = "0000-0001-7513-6760")
-    )),
-    "https://orcid.org/0000-0001-7513-6760"
+  expect_equal(get_person_iri(p=person(given="Daniel", family="Antal",
+                                       role = "cre", comment=c(ORCID = "0000-0001-7513-6760"))),
+               "https://orcid.org/0000-0001-7513-6760"
   )
-  expect_equal(
-    get_person_iri(
-      p =
-        person(
-          given = "Edgar",
-          family = "Anderson",
-          role = "cre",
-          comment = c(VIAF = "http://viaf.org/viaf/6440526")
-        )
+  expect_equal(get_person_iri(p=
+    person(given="Edgar", family="Anderson",
+           role = "cre", comment=c(VIAF="https://viaf.org/viaf/6440526"))
     ),
-    c(VIAF = "http://viaf.org/viaf/6440526")
-  )
-  expect_equal(
-    get_person_iri(
-      p =
-        person(given = "Taylor",
-               family = "Swift",
-               role = "cre",
-               comment = c(ISNI = "https://isni.org/isni/0000000078519858"))
-    ),
-    c(ISNI = "https://isni.org/isni/0000000078519858")
-  )
+           c(VIAF = "https://viaf.org/viaf/6440526"))
+  expect_equal(get_person_iri(p=
+                                person(given="Taylor", family="Swift", role = "cre", comment=c(ISNI="https://isni.org/isni/0000000078519858"))
+  ),
+  c(ISNI = "https://isni.org/isni/0000000078519858"))
 })
+
+test_that("create_iri() handles scalar types correctly", {
+  # Numeric (double)
+  expect_equal(create_iri(3.14), '"3.14"^^<http://www.w3.org/2001/XMLSchema#double>')
+
+  # Integer
+  expect_equal(create_iri(as.integer(42)), '"42"^^<http://www.w3.org/2001/XMLSchema#integer>')
+
+  # Date
+  expect_equal(create_iri(as.Date("2024-01-01")), '"2024-01-01"^^<http://www.w3.org/2001/XMLSchema#date>')
+
+  # POSIXct
+  dt <- as.POSIXct("2024-01-01 12:34:56", tz = "UTC")
+  expect_equal(create_iri(dt), '"2024-01-01T12:34:56Z"^^<xs:dateTime>')
+
+  # URI string
+  expect_equal(create_iri("https://example.org/id"), '<https://example.org/id>')
+
+  # Language-tagged literal
+  expect_equal(create_iri('"Some value"@en'), '"\"Some value\"@en"')
+
+  # Blank node (now unquoted)
+  expect_equal(create_iri("_:anon1"), "_:anon1")
+
+  # RDF type shortcut
+  expect_equal(create_iri("a"), "<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>")
+
+  # DCMI type abbreviation
+  expect_equal(
+    create_iri("DCMITYPE:Dataset"),
+    '"http://purl.org/dc/dcmitype/Dataset"^^<http://www.w3.org/2001/XMLSchema#string>'
+  )
+
+  # Plain string
+  expect_equal(create_iri("plain string"), '"plain string"^^<http://www.w3.org/2001/XMLSchema#string>')
+})
+
+test_that("create_iri() handles person() objects correctly", {
+  # Person with VIAF
+  p1 <- person("Jane", "Doe", comment = c(viaf = "12345"))
+  expect_equal(create_iri(p1), "<https://viaf.org/viaf/12345>")
+
+  # Person with ORCID
+  p2 <- person("J", "Doe", comment = c(orcid = "0000-0001-2345-6789"))
+  expect_equal(create_iri(p2), "<https://orcid.org/0000-0001-2345-6789>")
+})
+
+test_that("create_iri() fallback person gets a string literal", {
+  p3 <- person("Harper", "Lee", role = "aut", email = "test@example.com")
+  iri_p3 <- create_iri(p3)
+
+  expect_match(iri_p3, '^"[^"]+"\\^\\^<http://www.w3.org/2001/XMLSchema#string>$')
+  expect_true(grepl("Harper|Lee", iri_p3))
+})
+
+test_that("create_iri() fails for vector inputs", {
+  expect_error(create_iri(c("a", "b")), "input must be a scalar value")
+})
+
+test_that("create_iri() fails for list-like or tabular inputs", {
+  expect_error(create_iri(list(a = 1)), "must be a scalar")
+  expect_error(create_iri(data.frame(x = 1)), "must be a scalar")
+})
+
+
+test_that("prov_author() handles different person identifiers correctly", {
+  # ORCID
+  p_orcid <- person("Jane", "Doe", comment = c(ORCID = "0000-0002-1825-0097"))
+  triple_orcid <- prov_author(p_orcid)
+  expect_match(triple_orcid, "<https://orcid.org/0000-0002-1825-0097> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/ns/prov#Agent> \\.")
+
+  # ISNI
+  p_isni <- person("Alan", "Smith", comment = c(ISNI = "0000000121032683"))
+  triple_isni <- prov_author(p_isni)
+  expect_match(triple_isni, "<https://isni.org/isni/0000000121032683> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/ns/prov#Agent> \\.")
+
+  # VIAF
+  p_viaf <- person("Nick", "Carraway", comment = c(VIAF = "12345678"))
+  triple_viaf <- prov_author(p_viaf)
+  expect_match(triple_viaf, "<https://viaf.org/viaf/12345678> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/ns/prov#Agent> \\.")
+
+  # Wikidata
+  p_qid <- person("Fitzgerald", "Scott", comment = c(wikidata = "Q39232"))
+  triple_qid <- prov_author(p_qid)
+  expect_match(
+    triple_qid,
+    "^<https://www\\.wikidata\\.org/wiki/Q39232> <http://www\\.w3\\.org/1999/02/22-rdf-syntax-ns#type> <http://www\\.w3\\.org/ns/prov#Agent> \\.$"
+  )
+
+  # Fallback to blank node with label
+  p_fallback <- person("Harper", "Lee", role = "aut")
+  triple_fallback <- prov_author(p_fallback)
+  expect_match(triple_fallback,
+               "^_:[a-z]+ <http://www\\.w3\\.org/1999/02/22-rdf-syntax-ns#type> <http://www\\.w3\\.org/ns/prov#Agent> \\.$"
+  )
+
+  # Multiple persons
+  multi <- personList(p_orcid, p_isni)
+  triples_multi <- prov_author(multi)
+  expect_length(triples_multi, 2)
+  expect_true(all(grepl("^<https://", triples_multi)))
+})
+
